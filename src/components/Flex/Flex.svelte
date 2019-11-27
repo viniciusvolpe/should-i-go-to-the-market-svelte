261c04bc9d2bec529d40d1cb3171e54cc659dd0d
Vinicius Souza <vinicius.souza@aurum.com.br>
2019-11-27 18:11:07 -0300

    Adding image to home when has all items

---
+++ src/components/Flex/Flex.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let justifyContentCenter;
+  export let directionColumn;
+  export let alignItemsCenter;
+</script>
+<style>
+  .container {
+    display: flex;
+  }
+  .justifyContentCenter{
+    justify-content: center
+  }
+  .directionColumn{
+    flex-direction: column
+  }
+  .alignItemsCenter {
+    align-items: center
+  }
+</style>
+<div class="container" class:justifyContentCenter class:directionColumn class:alignItemsCenter>
+  <slot></slot>
+</div>
+
--- src/pages/Home/Home.svelte
+++ src/pages/Home/Home.svelte
@@ -1,1 +1,20 @@
-<h1>Home</h1>
+<script>
+  import Flex from "../../components/Flex/Flex.svelte"
+  let isFull = true
+</script>
+<style>
+  div {
+    margin-top: 20px;
+  }
+  h3 {
+    text-align: center
+  }
+</style>
+<div>
+  <Flex alignItemsCenter directionColumn class="container">
+    {#if isFull}
+      <img src="images/full.png" alt="full of ingredients"/>
+      <h3>Greate, you have all you need. Let's cook</h3>
+    {/if}
+  </Flex>
+</div>
